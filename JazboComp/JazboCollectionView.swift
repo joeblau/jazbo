147606bce5dcf8ca7ac4a080032c9a7be7aaa720
Joe Blau <im@joeblau.com>
2020-11-02 16:16:54 -0800

    Two sections types

--- JazboComp/JazboCollectionView.swift
+++ JazboComp/JazboCollectionView.swift
@@ -7,22 +7,61 @@
 
 import UIKit
 
+
+enum SectionLayoutKind: Int {
+    case grid
+    case list
+}
+
+
 class JazboCollectionView: UICollectionView {
     init() {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                              heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnv: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95),
-                                               heightDimension: .fractionalWidth(0.7))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
-                                                       subitems: [item])
+                
+            switch sectionLayoutKind {
+            case .grid:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalHeight(1.0))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
 
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        let layout = UICollectionViewCompositionalLayout(section: section)
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95),
+                                                       heightDimension: .fractionalWidth(0.7))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                               subitems: [item])
 
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                
+                return section
+            case .list:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalWidth(1.0))
+                    
+                let groupSize = itemSize
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                               subitem: item,
+                                                               count:1)
+                group.interItemSpacing = .fixed(10)
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 5
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                                leading: 5,
+                                                                bottom: 5,
+                                                                trailing: 5)
+                return section
+            }
+        }
+
+        
+        
+        
         super.init(frame: .zero, collectionViewLayout: layout)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemBackground
